d2d6fc09f8aa255ef227da25b3a69beae411b04b
Yuliya Runova <jul.runova@gmail.com>
2022-12-02 16:36:39 +0100

    feat: complete streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,10 @@
+import fs from "fs";
+import path from "path";
+
 const write = async () => {
-    // Write your code here 
+  let readStream = process.stdin;
+  let writeStream = fs.createWriteStream(path.resolve(process.cwd(), "./src/streams/files/fileToWrite.txt"));
+  readStream.pipe(writeStream);
 };
 
 await write();
