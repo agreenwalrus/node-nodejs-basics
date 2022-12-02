be443904d71cce61652d579ffed7b8ea622210f9
Yuliya Runova <jul.runova@gmail.com>
2022-12-02 14:15:48 +0100

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
+import fs from "node:fs/promises";
+import path from "path";
+const { createHash } = await import("node:crypto");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const pathFile = path.resolve(process.cwd(), "./src/hash/files/fileToCalculateHashFor.txt");
+  const data = await fs.readFile(pathFile, { encoding: "utf8" });
+  console.log(createHash("sha256").update(data).digest("hex"));
 };
 
 await calculateHash();
