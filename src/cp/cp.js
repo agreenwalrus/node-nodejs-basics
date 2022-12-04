742a23d8fd23e6fbf1b9384985046728ac4d5d68
Yuliya Runova <jul.runova@gmail.com>
2022-12-04 17:49:26 +0100

    feat: child process task is done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,10 @@
+import cp from "child_process";
+import path from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = cp.fork(path.join(process.cwd(), "./src/cp/files/script.js"), args.slice(2), { stdio: [null, "pipe", "pipe", "ipc"] });
+  child.stdout.pipe(process.stdout);
+  process.stdin.pipe(child.stdin);
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv);
