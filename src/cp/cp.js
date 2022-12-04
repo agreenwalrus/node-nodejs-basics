7bc974b0d24286aa87fe4c8b5736826b94437ef8
Yuliya Runova <jul.runova@gmail.com>
2022-12-04 22:37:11 +0100

    fix: pipe stdin. Add message for the user

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -2,9 +2,18 @@ import cp from "child_process";
 import path from "path";
 
 const spawnChildProcess = async (args) => {
-  const child = cp.fork(path.join(process.cwd(), "./src/cp/files/script.js"), args.slice(2), { stdio: [null, "pipe", "pipe", "ipc"] });
-  child.stdout.pipe(process.stdout);
+  const child = cp.fork(path.join(process.cwd(), "./src/cp/files/script.js"), args.slice(2), { stdio: ["pipe", "pipe", "pipe", "ipc"] });
   process.stdin.pipe(child.stdin);
+
+  let isFirstRun = true;
+  child.stdout.on("data", (data) => {
+    process.stdout.write(data);
+    if (!isFirstRun) {
+      process.stdout.write("Type in anything: ");
+    } else {
+      isFirstRun = false;
+    }
+  });
 };
 
 spawnChildProcess(process.argv);
